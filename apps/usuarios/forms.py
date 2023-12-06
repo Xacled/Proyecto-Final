d49a990f53c3780f25da390ec4ba87bf28b8d134
Carlos Daniel <carlox7v7@gmail.com>
2023-12-06 16:03:59 -0300

    Arreglar Registro

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -16,3 +16,64 @@ class RegistroForm(UserCreationForm):
             'password1',
             'password2'
         ]
+
+
+
+#      <!--
+# <p>
+#   <label for="id_last_name">Apellido:</label>
+#   <input type="text" name="last_name" maxlength="150" id="id_last_name" />
+# </p>
+
+# <p>
+#   <label for="id_username">Nombre de usuario:</label>
+#   <input type="text" name="username" maxlength="150" autofocus required id="idusername" />
+
+#   <span class="helptext"
+#     >Obligatorio. Longitud máxima de 150 caracteres. Solo puede estar formado
+#     por letras, números y los caracteres @/./+/-/.</span>
+# </p>
+
+# <p>
+#   <label for="id_email">Dirección de email:</label>
+#   <input type="email" name="email" maxlength="254" id="id_email" />
+# </p>
+
+# <p>
+#   <label for="id_password1">Contraseña:</label>
+#   <input
+#     type="password"
+#     name="password1"
+#     autocomplete="new-password"
+#     required
+#     id="id_password1"
+#   />
+
+#   <span class="helptext"
+#     ><ul>
+#       <li>
+#         Su contraseña no puede ser similar a otros componentes de su información
+#         personal.
+#       </li>
+#       <li>Su contraseña debe contener por lo menos 8 caracteres.</li>
+#       <li>Su contraseña no puede ser una contraseña usada muy comúnmente.</li>
+#       <li>Su contraseña no puede estar formada exclusivamente por números.</li>
+#     </ul></span
+#   >
+# </p>
+
+# <p>
+#   <label for="id_password2">Confirmación de contraseña:</label>
+#   <input
+#     type="password"
+#     name="password2"
+#     autocomplete="new-password"
+#     required
+#     id="id_password2"
+#   />
+
+#   <span class="helptext"
+#     >Introduzca la misma contraseña nuevamente, para poder verificar la
+#     misma.</span
+#   >
+# </p> -->
