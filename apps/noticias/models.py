f8f8ca022502097f9e21d15a6c9c5c79ffcc70f3
Agustin <agustinmuller98@gmail.com>
2023-12-10 21:44:54 -0300

    agregue filtro por mas vistos

--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -2,28 +2,31 @@ from django.db import models
 
 # Create your models here.
 
-class Categoria(models.Model):
-	nombre = models.CharField(max_length = 100)
 
-	def __str__(self):
-		return self.nombre
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
 
 class Noticia(models.Model):
 
-	creado = models.DateTimeField(
-		'creado',
-		auto_now_add=True
-	)
-	modificado = models.DateTimeField(
-		'modificado',
-		auto_now=True
-	)
-	titulo = models.CharField(max_length = 250)
-	contenido = models.TextField()
-	#para usar ImegeField, necesito tener instalado la libreria pillow
-	#pip install pillow
-	imagen = models.ImageField(upload_to = 'noticias')
-	categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
+    creado = models.DateTimeField(
+        'creado',
+        auto_now_add=True
+    )
+    modificado = models.DateTimeField(
+        'modificado',
+        auto_now=True
+    )
+    titulo = models.CharField(max_length=250)
+    contenido = models.TextField()
+    # para usar ImegeField, necesito tener instalado la libreria pillow
+    # pip install pillow
+    imagen = models.ImageField(upload_to='noticias')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    visitas = models.PositiveIntegerField(default=0)
 
-	def __str__(self):
-		return self.titulo
+    def __str__(self):
+        return self.titulo
--- apps/noticias/views.py
+++ apps/noticias/views.py
@@ -1,39 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Noticia, Categoria
 from .forms import Formulario_Noticia, Formulario_Modificar_Noticia
 
-#CONTROLA SI EL USUARIO ESTA LOGEADO EN UNA VISTA BASADA EN CLASES
+# CONTROLA SI EL USUARIO ESTA LOGEADO EN UNA VISTA BASADA EN CLASES
 from django.contrib.auth.mixins import LoginRequiredMixin
-#CONTROLA SI EL USUARIO ESTA LOGEADO EN UNA VISTA BASADA EN FUNCIONEs
+# CONTROLA SI EL USUARIO ESTA LOGEADO EN UNA VISTA BASADA EN FUNCIONEs
 from django.contrib.auth.decorators import login_required
 
-#CONTROLA QUE EL USUARIO SEA STAFF VISTA BASADA EN FuNCION
+# CONTROLA QUE EL USUARIO SEA STAFF VISTA BASADA EN FuNCION
 from django.contrib.admin.views.decorators import staff_member_required
-#CONTROLA QUE EL USUARIO SEA STAFF PARA VISTA BASADA EN CLASE
-from django.contrib.auth.mixins    import UserPassesTestMixin
+# CONTROLA QUE EL USUARIO SEA STAFF PARA VISTA BASADA EN CLASE
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 
-#Vasta Basada en funciones
+# Vasta Basada en funciones
 def Home_Noticias(request):
-	contexto = {}
-	cat = Categoria.objects.all()
-	contexto['categorias'] = cat
+    contexto = {}
+    cat = Categoria.objects.all()
+    contexto['categorias'] = cat
 
-	filtro = request.GET.get('categoria',None)
-	if not filtro or filtro == '0':
-		todas = Noticia.objects.all()
-	else:
-		categoria_seleccionada = Categoria.objects.get(pk = filtro)
-		todas = Noticia.objects.filter(categoria = categoria_seleccionada)
+    filtro = request.GET.get('categoria', None)
+    orden = request.GET.get('orden', None)
 
-	
-	contexto['noticias'] = todas
-	return render(request, 'noticias/home_noticias.html', contexto)
+    if not filtro or filtro == '0':
+        todas = Noticia.objects.all()
+    else:
+        categoria_seleccionada = Categoria.objects.get(pk=filtro)
+        todas = Noticia.objects.filter(categoria=categoria_seleccionada)
 
-#ACLARACION
+    if orden == 'visitas':
+        todas = todas.order_by('-visitas')
+    else:
+        todas = todas.order_by('-creado')
+    contexto['noticias'] = todas
+    return render(request, 'noticias/home_noticias.html', contexto)
+
+
+# ACLARACION
 '''
 si bien en la vista armo un diccionario del tipo
 {noticias: todas_noticias, fecha: '28-11-2023'}
@@ -44,43 +50,54 @@ noticias = todas_noticias
 fecha = '28-11-2023'
 '''
 
-#VISTA BASADA EN CLASE
+# VISTA BASADA EN CLASE
+
+
 class Home_Noticias_clase(ListView):
-	model = Noticia
-	template_name = 'noticias/home_noticias.html'
-	context_object_name = 'noticias'
+    model = Noticia
+    template_name = 'noticias/home_noticias.html'
+    context_object_name = 'noticias'
+
 
 class Cargar_noticia(CreateView):
-	model = Noticia
-	template_name = 'noticias/cargar_noticia.html'
-	form_class = Formulario_Noticia
-	success_url = reverse_lazy('noticias:h_noticias')
+    model = Noticia
+    template_name = 'noticias/cargar_noticia.html'
+    form_class = Formulario_Noticia
+    success_url = reverse_lazy('noticias:h_noticias')
+
 
 class Modificar_noticia(UpdateView):
-	model = Noticia
-	template_name = 'noticias/modificar_noticia.html'
-	form_class = Formulario_Modificar_Noticia
-	success_url = reverse_lazy('noticias:h_noticias')
+    model = Noticia
+    template_name = 'noticias/modificar_noticia.html'
+    form_class = Formulario_Modificar_Noticia
+    success_url = reverse_lazy('noticias:h_noticias')
+
 
 class Borrar_noticia(DeleteView):
-	model = Noticia
-	success_url = reverse_lazy('noticias:h_noticias')
+    model = Noticia
+    success_url = reverse_lazy('noticias:h_noticias')
 
-#ORM
+# ORM
 # CONSULTA PARA TRAER TODOS LOS DATOS
 # select * from Noticia  SQL
 # Noticia.objects.all()   ORM
 
-#CONSuLTA PARA TRAER SOLO UN DATO (POR CLAVE)
+# CONSuLTA PARA TRAER SOLO UN DATO (POR CLAVE)
 # select * from Noticia where id = algo    SQL
 # Noticia.objects.get(id = algo)		ORM
 
-#CONSuLTA PARA TRAER SOLO Algunos datos (POR filtro)
+# CONSuLTA PARA TRAER SOLO Algunos datos (POR filtro)
 # select * from Noticia where categororia = deportes
 # Noticia.objects.filter(categoria = deportes)
 
+
 def Detalle_noticia(request, pk):
-	ctx = {}
-	n = Noticia.objects.get(pk = pk)
-	ctx['noticia'] = n
-	return render(request,'noticias/detalle_noticia.html', ctx)
+    ctx = {}
+    noticia = get_object_or_404(Noticia, pk=pk)
+
+    # Incrementar el contador de visitas
+    noticia.visitas += 1
+    noticia.save()
+
+    ctx['noticia'] = noticia
+    return render(request, 'noticias/detalle_noticia.html', ctx)
