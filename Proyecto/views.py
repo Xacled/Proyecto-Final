dc0e65743783d58d5499376a3cb11a21072d086c
Agustin <agustinmuller98@gmail.com>
2023-12-12 15:05:58 -0300

    populares

--- Proyecto/views.py
+++ Proyecto/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from apps.noticias.models import Noticia
 
 def Home(request):
+	ctx = {}
+	noticias_populares = Noticia.objects.order_by('-visitas')[:3]  # Obtén las 5 noticias más populares
+	ctx['noticias_populares'] = noticias_populares
+	return render(request, 'home.html', ctx)
 
-	return render(request,'home.html')
 
 def Contacto(request):
 
