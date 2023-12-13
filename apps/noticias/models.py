591df5851ee49b290e6b6e00c5e7e86c6bbd1690
Carlos Daniel <carlox7v7@gmail.com>
2023-12-12 21:30:37 -0300

    Pequeños arreglos

--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
+
+
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
 
@@ -9,28 +11,24 @@ class Categoria(models.Model):
 
 
 class Noticia(models.Model):
-
-    creado = models.DateTimeField(
-        'creado',
-        auto_now_add=True
-    )
-    modificado = models.DateTimeField(
-        'modificado',
-        auto_now=True
-    )
+    
+    creado = models.DateTimeField("creado", auto_now_add=True)
+    modificado = models.DateTimeField("modificado", auto_now=True)
     titulo = models.CharField(max_length=250)
     contenido = RichTextField()
     # para usar ImegeField, necesito tener instalado la libreria pillow
     # pip install pillow
-    imagen = models.ImageField(upload_to='noticias')
+    imagen = models.ImageField(upload_to="noticias")
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     visitas = models.PositiveIntegerField(default=0)
-    likes = models.ManyToManyField(User, related_name="not_megustas",blank=True)
+    likes = models.ManyToManyField(User, related_name="not_megustas", blank=True)
 
     def count_likes(self):
         return self.likes.count()
+
     def __str__(self):
         return self.titulo
+
 
 class Comentario(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
