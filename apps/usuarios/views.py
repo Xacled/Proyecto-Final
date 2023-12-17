0cea11f5cebbae373057cfba89a33ef2f6375a95
Agustin <agustinmuller98@gmail.com>
2023-12-17 20:26:12 -0300

    ver posts

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render ,get_object_or_404
 from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
-
+from apps.noticias.models import Noticia
 from .forms import RegistroForm, Formulario_Modificar_Usuario
 from .models import CustomUser
 # Create your views here.
@@ -9,8 +9,14 @@ from .models import CustomUser
 
 
 def Ver_perfil(request, username):
+    ctx = {}
     user = get_object_or_404(CustomUser, username=username)
-    return render(request, 'usuarios/perfil.html', {'user': user})
+    noticias_user = Noticia.objects.filter(usuario = user)  # Obtén las 5 noticias más populares
+    ctx['noticias_user'] = noticias_user
+    ctx['user'] = user
+    return render(request, 'usuarios/perfil.html', ctx)
+
+
 
 class Registro(CreateView):
 	#FORMULARIO DJANGO
@@ -22,5 +28,8 @@ class Modificar_usuario(UpdateView):
     model = CustomUser
     template_name = 'usuarios/modificar_perfil.html'
     form_class = Formulario_Modificar_Usuario
-    success_url = reverse_lazy('noticias:home_noticias')
+    def get_success_url(self):
+        username = self.object.username
+        return reverse_lazy('usuarios:ver_perfil', kwargs={'username': username})
+   
 
