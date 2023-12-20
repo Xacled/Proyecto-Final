70ab5b36fccbfdc9093994e7000c23043bb10954
Agustin <agustinmuller98@gmail.com>
2023-12-20 17:14:58 -0300

    .

--- Proyecto/settings/base.py
+++ Proyecto/settings/base.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from django.urls import reverse_lazy
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,6 +130,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER ='blog.info.cambio.climatico@gmail.com'
+EMAIL_HOST_PASSWORD = 'twlr jxwi dihu eozj'
 EMAIL_USE_SSL = False
